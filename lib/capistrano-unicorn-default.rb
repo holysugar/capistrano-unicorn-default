dcc5e3d06f81c7d5bc7e18dfe14b30fc3fa571c7
holy <holysugar@gmail.com>
2012-10-25 22:02:41 +0900

    initial commit

---
+++ lib/capistrano-unicorn-default.rb
@@ -0,0 +1,9 @@
+require "capistrano-unicorn-default/version"
+
+module Capistrano
+  module Unicorn
+    module Default
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano-unicorn-default/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Unicorn
+    module Default
+      VERSION = "0.0.1"
+    end
+  end
+end
